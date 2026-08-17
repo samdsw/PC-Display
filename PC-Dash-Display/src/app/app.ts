63735c919469a3093ffa02c9f3f7202598d08bce
Sam Will <samwill@email.com>
2026-08-17 19:34:10 -0400

    Random things added... can't remember

--- PC-Dash-Display/src/app/app.ts
+++ PC-Dash-Display/src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home';
+import { HeaderComponent } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, HeaderComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
   protected readonly title = signal('pc-dash-display');
