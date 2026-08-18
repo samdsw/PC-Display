800660ce1ee961a61bd5c51d54548c1a01ba2a59
Sam Will <samwill@email.com>
2026-08-17 20:23:20 -0400

    Quick add metrics

--- PC-Dash-Display/src/app/home/home.ts
+++ PC-Dash-Display/src/app/home/home.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
 
+interface SystemMetric {
+  name: string;
+  usage: number;
+  detail: string;
+}
+
 @Component({
   selector: 'app-home',
   imports: [],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
+
 export class HomeComponent {
-  
+  metrics: SystemMetric[] = [
+    { name: 'CPU', usage: 45, detail: '65°C' },
+    { name: 'GPU', usage: 70, detail: '75°C' },
+    { name: 'RAM', usage: 60, detail: '8GB / 32GB' },
+  ];  
+
+  refreshMetrics() {
+    // Logic to refresh system metrics goes here
+    
+  }
 }
