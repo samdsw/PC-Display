f28249dcb197e70ee1fb52cceecd0be3d8efabf5
Sam Will <samwill@email.com>
2026-08-23 19:11:02 -0400

    Updating PC stats, the header, and Current time/date

--- PC-Dash-Display/src/app/components/header/header.ts
+++ PC-Dash-Display/src/app/components/header/header.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   imports: [],
   templateUrl: './header.html',
-  styleUrl: './header.css',
+  styleUrls: ['./header.css'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+  currentTime = '';
+  currentDate = '';
 
+  private clockTimerId?: ReturnType<typeof setInterval>;
+
+  ngOnInit() {
+    this.updateClock();
+
+    this.clockTimerId = setInterval(() => {
+      this.updateClock();
+    }, 1000);
+  }
+
+  ngOnDestroy(): void {
+    if (this.clockTimerId) {
+      clearInterval(this.clockTimerId);
+    }
+  }
+
+  private updateClock() {
+    const now = new Date();
+
+    this.currentTime = now.toLocaleTimeString('en-US', {
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+    });
+    
+    this.currentDate = now.toLocaleDateString('en-US', {
+      weekday: 'long',
+      month: 'long',
+      day: 'numeric',
+    });
+  }
 }
